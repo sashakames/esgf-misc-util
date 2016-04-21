ea1b96dce4a23a4e1b2cfc3ae1fc01b2e6d951a9
Sasha Ames <ames4@aims3.llnl.gov>
2016-04-21 16:05:46 -0700

    fixing up

--- access-logs/run_log_fill.sh
+++ access-logs/run_log_fill.sh
@@ -3,10 +3,9 @@ export PGPASSWORD=`cat /esg/config/.esg_pg_pass`
 source /etc/esg.env
 
 
-fn=`ls -trl /usr/local/tomcat/logs/localhost_access_log..20*.txt | tail -n 1 | awk '{print $9}'`
+fn=`ls -trl /usr/local/tomcat/logs/localhost_access_log..20*.txt | tail -n 2 | head -n 1 | awk '{print $9}'`
+echo $fn
 
+python /opt/esgf/util/backfill_logs.py $fn  > /tmp/logtmp.sql
 
-echo time python /opt/esgf/util/backfill_logs.py $fn 
-# > /tmp/logtmp.sql
-
-echo time psql -U dbsuper esgcet < /tmp/logtmp.sql
+psql -U dbsuper esgcet < /tmp/logtmp.sql
