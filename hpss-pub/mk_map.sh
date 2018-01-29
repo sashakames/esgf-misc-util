06642669c621a5a029f9dfc30ccc694f1c925bf0
Sasha Ames <amysash2006@gmail.com>
2018-01-29 10:16:31 -0800

    update arguments comment and server variable

--- hpss-pub/mk_map.sh
+++ hpss-pub/mk_map.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# script to generate mapfiles for publishing 
+
+# arguments <PATH> <PROJECT> <DATASET_ID_SUFFIX> 
+# $1 = PATH
+# ...
+
+SERVER=""
+
 path=`./urlencode.py $1`
 
 project=$2
 
 digest=`./gen_digest.py $path`
 
-url="https://pcmdi11.llnl.gov/basej/metadata?path=$path&digest=$digest"
+url="https://$SERVER/basej/metadata?path=$path&digest=$digest"
 
 #echo $url
 
@@ -16,10 +24,11 @@ key=`echo $resp | ./parsekey.py`
 
 #echo $key
 
-exurl="https://pcmdi11.llnl.gov/basej/expose/$2?path=$path&key=$key"
+exurl="https://$SERVER/basej/expose/$2?path=$path&key=$key"
 
 #echo $exurl
 
 #curl -X POST -k $exurl
 
 echo $resp | ./json2map.py $project $project.$3
+
